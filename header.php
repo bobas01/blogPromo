57aace061f8dbabd4a9a57341450e36aca4b8848
Sequeira Alexandre <67326944+bobas01@users.noreply.github.com>
2023-08-24 08:17:04 +0200

    404.html finished

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/style.css">
+    <link rel="stylesheet" href="./asset/font/font.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <div id="logoBurger">
+             <a id="logo" href="./index.php"><img src="./asset/img/Logo180.png" alt="logo"></a> 
+    <img id="menuBurger" src="./asset/img/iconBurger.png" alt="menu">
+        </div>
+   
+    <nav>
+       
+        <ul id="menu">
+            <li><a href="./stagiaires">Les stagiaires</a></li>
+            <li><a href="./actuality">Les actualités</a></li>
+            <li><a href="./category">Les catégories</a></li>
+            
+        </ul>
+    </nav>
+    </header>
+    <script src="./asset/js/main.js"></script>
+</body>
+</html>
